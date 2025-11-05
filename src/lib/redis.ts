afd1f07e0494bfbc79720794af68b2d9def52136
appointease.dev <appointease.dev@gmail.com>
2025-11-05 17:20:57 +0000

    Correct

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,19 +1,12 @@
 import { env } from '@/env.mjs';
 import { createClient } from 'redis';
 
-export const redisClient = env.REDIS_URI
-    ? createClient({ url: env.REDIS_URI })
-    : {
-          connect: async () => {},
-          ping: async () => 'PONG',
-          get: async (key: string) => null,
-          set: async (key: string, value: string) => {},
-          hSet: async (key: string, values: Record<string, string>) => {},
-          hGet: async (key: string, field: string) => {},
-          del: async (key: string) => {},
-      };
-
-if (env.REDIS_URI) {
-    await redisClient.connect();
-    await redisClient.ping();
-}
+export const redisClient = {
+    connect: async () => {},
+    ping: async () => 'PONG',
+    get: async (key: string) => null,
+    set: async (key: string, value: string) => {},
+    hSet: async (key: string, values: Record<string, string>) => {},
+    hGet: async (key: string, field: string) => {},
+    del: async (key: string) => {},
+};
